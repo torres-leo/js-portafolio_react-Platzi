dedd6bc560133ea93f7032e7c6f80e118064102f
Leo Torres <leojts24@gmail.com>
2022-03-11 14:39:03 -0600

    Añadiendo el archivo netlify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl';
 
-console.log('Hola watch');
 (async function App() {
 	const main = null || document.getElementById('main');
 	main.innerHTML = await Template();
